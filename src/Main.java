0631eda12a210800f52a7b9d5e8d9c98dc89714e
yarik <yarikshtoda1988@gmail.com>
2022-12-23 21:35:36 +0300

    Дз Коллекции :hash Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[ ] args) {
+        Random random = new Random();
+        Set<Integer> numbers = new HashSet<>();
+
+        while (numbers.size() < 20) {
+            numbers.add(random.nextInt(1000));
+        }
+        System.out.println(numbers);
+
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            Integer number = iterator.next();
+            if (number  %2 ==1) {
+                iterator.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 
 }
